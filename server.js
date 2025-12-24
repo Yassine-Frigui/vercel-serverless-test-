36b7afaa24055400344e6ac3851c20feb5b5415a
Youssef_Frigui <youssef.frigui13@gmail.com>
2025-12-24 14:53:23 +0100

    auto_dep func

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+var date = new Date();
+
 app.get('/', (req, res) => {
-  res.send('test pings !');
+
+
+  res.send('auto deploy at :  ' + date.toISOString());
 });
 
 app.listen(port, () => {
